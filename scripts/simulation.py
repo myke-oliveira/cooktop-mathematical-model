da3140da71ef8023357ceb595125851a5b298dc6
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-31 19:54:37 -0300

    Refactor simulation.py

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -10,7 +10,7 @@ R = 5e-2
 N = 10
 e = 5e-3
 d = 1e-2
-numsteps = 100
+numsteps = 300
 
 # Variables
 i = 1.
@@ -37,16 +37,16 @@ plt.show(True)
 # # Elementos do condutor orientado
 
 #dx_L, dy_L dz_L: coordenadas dos elementos do contudor
-dx_L = x_L[1:] - x_L[:-1]
-dy_L = y_L[1:] - y_L[:-1]
-dz_L = np.zeros(len(dx_L))
+dX_L = x_L[1:] - x_L[:-1]
+dY_L = y_L[1:] - y_L[:-1]
+dZ_L = np.zeros(len(dX_L))
 
 # X_L, Y_L: coordenadas dos pontos médios dos elementos do condutor
 X_L = (x_L[1:] + x_L[:-1])/2
 Y_L = (y_L[1:] + y_L[:-1])/2
 
 fig, ax = plt.subplots()
-q = ax.quiver(X_L, Y_L, dx_L, dy_L)
+q = ax.quiver(X_L, Y_L, dX_L, dY_L)
 fig.show(True)
 
 x_P, y_P, z_P = 3e-2, 3e-2, 3e-2
@@ -55,10 +55,14 @@ Y_R = (y_P - y_0 for y_0 in y_L)
 Z_R = (y_P - z_0 for z_0 in z_L)
 # print(*X_R, *Y_R, *Z_R)
 
-for x_r, y_r, z_r in zip(X_R, Y_R, Z_R):
-    r_squared = x_r**2 + y_r**2 + z_r**2
-    x_r_hat = x_r / r_squared**5
-    y_r_hat = y_r / r_squared**5
-    z_r_hat = z_r / r_squared**5
+B = 0
+#for x_r, y_r, z_r, dx_L, dy_L, dz_L in zip(X_R, Y_R, Z_R, dX_L, dY_L, dZ_L):
+#    r_squared = x_r**2 + y_r**2 + z_r**2
+#    x_r_hat = x_r / r_squared**5
+#    y_r_hat = y_r / r_squared**5
+#    z_r_hat = z_r / r_squared**5
+#    dl = np.array((dx_L, dy_L, dz_L))
+#    r_hat = np.array((x_r_hat, y_r_hat, z_r_hat))
+    
 #    
 
